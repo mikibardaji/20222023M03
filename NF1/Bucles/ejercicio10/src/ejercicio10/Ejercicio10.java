f0ff97c3bb43344c0f84a2a732c09258d7656896
Miguel Angel Bardaji Bosch <mabardaji@proven.local>
2022-10-04 12:57:29 +0200

    bucles20221004

--- NF1/Bucles/ejercicio10/src/ejercicio10/Ejercicio10.java
+++ NF1/Bucles/ejercicio10/src/ejercicio10/Ejercicio10.java
@@ -4,6 +4,8 @@
  */
 package ejercicio10;
 
+import java.util.Scanner;
+
 /**
  *
  * @author mabardaji
@@ -14,7 +16,43 @@ public class Ejercicio10 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int nota;
+        boolean puesto_10 = false;
+         Scanner sc = new Scanner(System.in);
+         int cont_notas=0;
+        do
+            {
+                 System.out.println("Pon notas, escribe -1 para salir");
+                 nota=sc.nextInt();
+                 
+                 if (nota==10)
+                    { //activo variable centinela
+                        puesto_10=true;
+                    }
+                 if (nota>10 || nota<-1)
+                 {
+                     System.out.println("nota incorrrecta debe de estar entre 0 y 10");
+                 }
+                 else{ //nota correcta
+                     cont_notas++;
+                 }
+                
+            } while(nota!=-1);
+        
+        
+            if (puesto_10==true)
+                {
+                    System.out.println("Has puesto algun 10");
+                }
+            else
+            {
+                System.out.println("No has pueso ningun 10");
+            }
+        
+        System.out.println("Has pusto" + cont_notas + " notas");
+       
+        
+        
     }
     
 }
